5fe8855f36caa88216f2093c368c08e97776e753
chenjl0710 <32512198+chenjl0710@users.noreply.github.com>
2018-06-26 21:09:20 +0800

    admin
    update admin

--- work/admin.py
+++ work/admin.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+import datetime
+import time
 
 # Register your models here.
 from work.models import log, Project,weekReport,monthReport
@@ -10,7 +12,8 @@ from django.contrib.auth.admin import User
 #https://www.cnblogs.com/wumingxiaoyao/p/6928297.html
 @admin.register(log)
 class LogAdmin(admin.ModelAdmin):
-    # 添加Media是为了将textField的数入框变成富文本编辑框
+    # 添加Media是为了将textField的数入框变成富文本编辑框,
+    #需要在textarea.js文件里添加对应的函数
     class Media:
         css = {'all':(
             'work/css/simditor.css',
@@ -47,19 +50,99 @@ class LogAdmin(admin.ModelAdmin):
     actions_on_top = False   #将顶部的动作action功能关闭
 
     # search_fields = ('user', 'project')
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
+    # 添加Media是为了将textField的数入框变成富文本编辑框
+    class Media:
+        css = {'all':(
+            'work/css/simditor.css',
+        )}
+        js = (
+            # 'work/js/simditor/jquery.min.js',
+            'https://cdn.bootcss.com/jquery/3.2.1/jquery.js',
+            'work/js/simditor/module.js',
+            'work/js/simditor/uploader.js',
+            'work/js/simditor/hotkeys.js',
+            'work/js/simditor/simditor.js',
+            'work/js/rich_text.js',
+            'work/js/textarea.js'
+        )
 
     list_display = ('projectname', 'remarks')
 
 @admin.register(weekReport)
 class weekReportAdmin(admin.ModelAdmin):
+    # 添加Media是为了将textField的数入框变成富文本编辑框
+    class Media:
+        css = {'all':(
+            'work/css/simditor.css',
+        )}
+        js = (
+            # 'work/js/simditor/jquery.min.js',
+            'https://cdn.bootcss.com/jquery/3.2.1/jquery.js',
+            'work/js/simditor/module.js',
+            'work/js/simditor/uploader.js',
+            'work/js/simditor/hotkeys.js',
+            'work/js/simditor/simditor.js',
+            'work/js/rich_text.js',
+            'work/js/textarea.js'
+        )
 
-    list_display = ("weekNum", "weekCycle")
+    def getfirstday(self,weekflag):
+        # weekflag格式为201435（即2014年第35周）
+        #根据当前日期计算当前周的周日日期
+        yearnum = weekflag[0:4]  # 取到年份
+        weeknum = weekflag[4:6]  # 取到周
+        stryearstart = yearnum + '0101'  # 当年第一天
+        yearstart = datetime.datetime.strptime(stryearstart, '%Y%m%d')  # 格式化为日期格式
+        yearstartcalendarmsg = yearstart.isocalendar()  # 当年第一天的周信息
+        yearstartweek = yearstartcalendarmsg[1]
+        yearstartweekday = yearstartcalendarmsg[2]
+        yearstartyear = yearstartcalendarmsg[0]
+        if yearstartyear < int(yearnum):
+            daydelat = (8 - int(yearstartweekday)) + (int(weeknum) - 1) * 7
+        else:
+            daydelat = (8 - int(yearstartweekday)) + (int(weeknum) - 2) * 7
+        a = (yearstart + datetime.timedelta(days=daydelat)).date()
+        return a
+
+    def save_model(self, request, obj, form, change):
+        #在保存时默认user的值是当前登录的用户
+        obj.user = request.user
+        day = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+        week = datetime.datetime.strptime(day, "%Y-%m-%d").strftime("%W")
+        obj.weekNum = day.split("-")[0] + u" 第" + week + u"周"
+        monday = self.getfirstday(day.split("-")[0]+week)
+        friday = monday + datetime.timedelta(days = 4)
+        obj.weekCycle = str(monday) + "---" + str(friday) + u"周报"
+        super(weekReportAdmin,self).save_model(request, obj, form, change)
+
+    fields = ("lastWeekContent","thisWeekContent","nextWeekContent")#显示需要编辑的字段
+    list_display = ('user','weekNum','weekCycle')  #在显示列表里显示字段
+    list_filter = ('user','weekNum','weekCycle')  #右侧的过滤器
+    ordering = ('-weekCycle',)  #默认排序字段
+    # date_hierarchy = "weekCycle"   # 详细时间分层筛选　
+
+    actions_on_top = False   #将顶部的动作action功能关闭
 
 @admin.register(monthReport)
 class monthReportAdmin(admin.ModelAdmin):
-
+    # 添加Media是为了将textField的数入框变成富文本编辑框
+    class Media:
+        css = {'all':(
+            'work/css/simditor.css',
+        )}
+        js = (
+            # 'work/js/simditor/jquery.min.js',
+            'https://cdn.bootcss.com/jquery/3.2.1/jquery.js',
+            'work/js/simditor/module.js',
+            'work/js/simditor/uploader.js',
+            'work/js/simditor/hotkeys.js',
+            'work/js/simditor/simditor.js',
+            'work/js/rich_text.js',
+            'work/js/textarea.js'
+        )
     list_display = ("monthNum",)
 
 # admin.site.register(log, admin_class=LogAdmin)
