01ce2a14bede55bccce14420d52769ec65452f38
chenjl0710 <32512198+chenjl0710@users.noreply.github.com>
2018-06-26 20:41:14 +0800

    css js
     css js update

--- static/work/js/textarea.js
+++ static/work/js/textarea.js
@@ -1,11 +1,42 @@
-$(function() {
-    toolbar = [ 'title', 'bold', 'italic', 'underline', 'strikethrough',
-            'color', '|', 'ol', 'ul', 'blockquote', 'code', 'table', '|',
-            'link', 'image', 'hr', '|', 'indent', 'outdent' ];
-    var editor = new Simditor({
-        textarea: $("#id_content"),
-        placeholder: "在此编辑你的文章",
-        toolbar : toolbar,
-        //optional options
+(function () {
+    $(function() {
+        toolbar = [ 'title', 'bold', 'italic', 'underline', 'strikethrough',
+                'color', '|', 'ol', 'ul', 'blockquote', 'code', 'table', '|',
+                'link', 'image', 'hr', '|', 'indent', 'outdent' ];
+        var editor1 = new Simditor({
+            textarea: $("#id_content"),
+            placeholder: "在此编辑你的文章",
+            toolbar : toolbar,
+        });
     });
-});
+    $(function() {
+        toolbar = [ 'title', 'bold', 'italic', 'underline', 'strikethrough',
+                'color', '|', 'ol', 'ul', 'blockquote', 'code', 'table', '|',
+                'link', 'image', 'hr', '|', 'indent', 'outdent' ];
+        var editor2 = new Simditor({
+            textarea: $("#id_lastWeekContent"),
+            placeholder: "在此编辑你的文章",
+            toolbar : toolbar,
+        });
+    });
+    $(function() {
+        toolbar = [ 'title', 'bold', 'italic', 'underline', 'strikethrough',
+                'color', '|', 'ol', 'ul', 'blockquote', 'code', 'table', '|',
+                'link', 'image', 'hr', '|', 'indent', 'outdent' ];
+        var editor2 = new Simditor({
+            textarea: $("#id_thisWeekContent"),
+            placeholder: "在此编辑你的文章",
+            toolbar : toolbar,
+        });
+    });
+    $(function() {
+        toolbar = [ 'title', 'bold', 'italic', 'underline', 'strikethrough',
+                'color', '|', 'ol', 'ul', 'blockquote', 'code', 'table', '|',
+                'link', 'image', 'hr', '|', 'indent', 'outdent' ];
+        var editor2 = new Simditor({
+            textarea: $("#id_nextWeekContent"),
+            placeholder: "在此编辑你的文章",
+            toolbar : toolbar,
+        });
+    });
+}).call(this);
